ffdf8aabad3ee8a755289847f4f125d47f67d7d0
BurhanYasir123 <burhanyasirburhan2@gmail.com>
2026-06-15 13:53:29 +0500

    Added triangle creator for testing

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -14,7 +14,6 @@ public:
 		frame_num++;
 
 		Saffron::RendererInitInfo info{};
-
 		info.window_width = 800;
 		info.window_height = 600;
 		info.window_title = "Saffron";
@@ -41,15 +40,40 @@ public:
 		    if(ev.IsKeyDown(Saffron::Key::D0)){ r.SetBackgroundColor({0.1f, 0.3f, 0.4f}); }
 		    else{ r.SetBackgroundColor({0.1f, 0.9f, 0.4f}); }
 
-		    glfwPollEvents();
-
 		    ImGuiIO& io = ImGui::GetIO();
+
+		    // --- ImGui Window for Triangle Creation ---
+    		static float p1[3] = { -0.5f, -0.5f, 0.0f };
+    		static float p2[3] = {  0.5f, -0.5f, 0.0f };
+    		static float p3[3] = {  0.0f,  0.5f, 0.0f };
+    		static float color[3] = { 0.4f, 0.7f, 0.2f };
+		
+    		ImGui::Begin("Triangle Creator");
+		
+    		ImGui::Text("Set Vertex Positions:");
+    		ImGui::SliderFloat3("Point 1", p1, -1.0f, 1.0f);
+    		ImGui::SliderFloat3("Point 2", p2, -1.0f, 1.0f);
+    		ImGui::SliderFloat3("Point 3", p3, -1.0f, 1.0f);
+		
+    		ImGui::ColorEdit3("Color", color);
+		
+    		if(ImGui::Button("Add Triangle")) {
+    		    Saffron::TriangleInfo info;
+    		    info.point1 = Saffron::Vertex({ p1[0], p1[1], p1[2] });
+    		    info.point2 = Saffron::Vertex({ p2[0], p2[1], p2[2] });
+    		    info.point3 = Saffron::Vertex({ p3[0], p3[1], p3[2] });
+    		    info.color = { color[0], color[1], color[2] };
+    		    r.InitTriangle(info);
+    		}
+		
+    		ImGui::End();
+
 
 		    if(frame_num == 1 || (frame_num%100) == 0) auto fps = 1.0f / io.DeltaTime;
 
     		ImGui::Begin("Demo");
     		ImGui::Text("FPS: %.1f", 1.0f / io.DeltaTime);
-    		ImGui::End();
+    		ImGui::End();    		
 
 		    r.EndFrame();
 		}
